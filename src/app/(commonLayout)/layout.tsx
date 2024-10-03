c43095010810eaf04c0effe9f8bd3f880b9da7e4
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-10-03 22:24:59 +0600

    Basic Layout created

---
+++ src/app/(commonLayout)/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Pawfect",
+  description:
+    "Your ultimate platform for pet care tips, stories, and nutrition advice. Explore and share knowledge to keep your pets happy and healthy.",
+};
+
+export default function CommonLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <div>{children}</div>;
+}
