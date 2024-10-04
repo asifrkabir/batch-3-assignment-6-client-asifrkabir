ecf52d35ebbcfd7ea4acbc93311e76db9adc0af4
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-10-04 11:51:22 +0600

    Login and Register pages created

---
+++ src/app/(commonLayout)/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/components/login/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
