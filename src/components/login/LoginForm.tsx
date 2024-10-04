d997c1b0197b89399fcfbb661a8c59f1204a5f53
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-10-04 20:25:58 +0600

    LoadingSpinner implemented

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -24,6 +24,8 @@ import { toast } from "sonner";
 import AppForm from "../form/AppForm";
 import AppInput from "../form/AppInput";
 import { Label } from "../ui/label";
+import React from "react";
+import LoadingSpinner from "../ui/LoadingSpinner/LoadingSpinner";
 
 const defaultValues = {
   email: "john.user4@example.com",
@@ -63,63 +65,66 @@ export function LoginForm() {
         router.push("/");
       }
     }
-  }, [isPending, isSuccess]);
+  }, [isPending, isSuccess, redirect, router]);
 
   return (
-    <Card className="mx-auto max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
-        <CardDescription>
-          Enter your credentials below to login to your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-          <AppForm
-            defaultValues={defaultValues}
-            onSubmit={onSubmit}
-            resolver={zodResolver(loginValidationSchema)}
-          >
-            <AppInput
-              name="email"
-              label="Email"
-              type="email"
-              placeholder="Enter your email"
-              required
-            />
-
-            <div>
-              <div className="flex items-center">
-                <Label htmlFor="password">
-                  Password <span className="text-red-500">*</span>
-                </Label>
-                <Link
-                  href="#"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  Forgot your password?
-                </Link>
-              </div>
+    <>
+      {isPending && <LoadingSpinner />}
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your credentials below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <AppForm
+              defaultValues={defaultValues}
+              onSubmit={onSubmit}
+              resolver={zodResolver(loginValidationSchema)}
+            >
               <AppInput
-                name="password"
-                type="password"
-                placeholder="Enter your password"
+                name="email"
+                label="Email"
+                type="email"
+                placeholder="Enter your email"
                 required
               />
-            </div>
 
-            <Button type="submit" className="w-full">
-              Login
-            </Button>
-          </AppForm>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <Link href="#" className="underline">
-            Sign up
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
+              <div>
+                <div className="flex items-center">
+                  <Label htmlFor="password">
+                    Password <span className="text-red-500">*</span>
+                  </Label>
+                  <Link
+                    href="#"
+                    className="ml-auto inline-block text-sm underline"
+                  >
+                    Forgot your password?
+                  </Link>
+                </div>
+                <AppInput
+                  name="password"
+                  type="password"
+                  placeholder="Enter your password"
+                  required
+                />
+              </div>
+
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+            </AppForm>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="#" className="underline">
+              Sign up
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </>
   );
 }
