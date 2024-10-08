d0baac2cbdac18f8f5b225b2385bead83b8379de
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-10-08 20:26:47 +0600

    User Management completed

--- src/hooks/user.hook.ts
+++ src/hooks/user.hook.ts
@@ -1,5 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { getUserById, updateUser } from "@/services/UserService";
+import {
+  deleteUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+} from "@/services/UserService";
+import { IQueryParam } from "@/types";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
 export const useGetUserById = (userId: string) => {
@@ -16,6 +22,28 @@ export const useUpdateUser = () => {
   return useMutation<any, Error, any>({
     mutationKey: ["UPDATE_USER"],
     mutationFn: async (userData) => await updateUser(userData),
+    onSuccess: (data) => {
+      return data;
+    },
+    onError: (error) => {
+      return error;
+    },
+  });
+};
+
+export const useGetAllUsers = (params?: IQueryParam[]) => {
+  return useQuery({
+    queryKey: ["GET_ALL_USERS"],
+    queryFn: async () => await getAllUsers(params),
+    refetchOnMount: "always",
+    staleTime: 0,
+  });
+};
+
+export const useDeleteUser = () => {
+  return useMutation<any, Error, any>({
+    mutationKey: ["DELETE_USER"],
+    mutationFn: async (userId) => await deleteUser(userId),
     onSuccess: (data) => {
       return data;
     },
--- src/services/UserService/index.ts
+++ src/services/UserService/index.ts
@@ -2,7 +2,7 @@
 "use server";
 
 import axiosInstance from "@/lib/AxiosInstance";
-import { IApiResponse, IUser } from "@/types";
+import { IApiResponse, IQueryParam, IUser } from "@/types";
 
 export const getUserById = async (userId: string) => {
   try {
@@ -28,6 +28,50 @@ export const updateUser = async (userData: any) => {
       `/users`,
       userData.formData,
       { headers: { "Content-Type": "multipart/form-data" } }
+    );
+
+    return data;
+  } catch (error: any) {
+    if (error.response) {
+      const responseData = error.response.data as IApiResponse<null>;
+
+      return responseData;
+    }
+
+    throw new Error(error.message || "Unknown error occurred");
+  }
+};
+
+export const getAllUsers = async (params?: IQueryParam[]) => {
+  try {
+    const queryParams = new URLSearchParams();
+
+    if (params) {
+      params.forEach((item) => {
+        queryParams.append(item.name, item.value as string);
+      });
+    }
+
+    const { data } = await axiosInstance.get<IApiResponse<IUser[]>>("/users", {
+      params: queryParams,
+    });
+
+    return data;
+  } catch (error: any) {
+    if (error.response) {
+      const responseData = error.response.data as IApiResponse<null>;
+
+      return responseData;
+    }
+
+    throw new Error(error.message || "Unknown error occurred");
+  }
+};
+
+export const deleteUser = async (userId: string) => {
+  try {
+    const { data } = await axiosInstance.delete<IApiResponse<IUser>>(
+      `/users/${userId}`
     );
 
     return data;
